6dbc1a42b075f506855d7e29f6ab37eb2297423c
Peter Soliman <63909532+Peter-Soliman1@users.noreply.github.com>
2022-06-26 15:31:47 +0200

    updating bash script comments

--- edit_user.sh
+++ edit_user.sh
@@ -2,6 +2,8 @@
 
 file='users.txt'
 
+# copy users content to a txt file 
+
 echo "John john@domain.com 325
 Susan 510
 Jane jane@domain.com 131
@@ -12,16 +14,17 @@ Bert bert@localhost 150
 IFS=$'\n'
 while read line; do
 
+#if condition to check that three records to be checked have all required details (name , email and ID)
     if [ $(wc -w <<< "$line") -eq 3 ] 
     then
         name=$(echo "$line" | awk '{print $1}')
         email=$(echo "$line" | awk '{print $2}')
         id=$(echo "$line" | awk '{print $3}')
+        # check if the record conatins valid FQDN (Email)
         fqdn="^(([A-Za-z0-9]+((\.|\-|\_|\+)?[A-Za-z0-9]?)*[A-Za-z0-9]+)|[A-Za-z0-9]+)@(([A-Za-z0-9]+)+((\.|\-|\_)?([A-Za-z0-9]+)+)*)+\.([A-Za-z]{2,})+$"
-        #if [[ $email == *"@domain.com"* ]]
         if [[ $email =~ ${fqdn} ]]
-        #if [ "$email" != "${email/.}" ]
         then
+        # Check whether ID is ODD or EVEN
             if [[ $id%2 -eq 0 ]]
             then
                 echo " $id of $email is even number"
