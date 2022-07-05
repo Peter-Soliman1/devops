ed1be318ce8a1a7be72db9474ab93b347b55e6b1
Peter Soliman <63909532+Peter-Soliman1@users.noreply.github.com>
2022-07-05 12:17:31 +0200

    delete become_user

--- edit_user.sh
+++ edit_user.sh
@@ -10,9 +10,11 @@ Jane jane@domain.com 131
 Karl karl@domain.com
 Bert bert@localhost 150
 jhf uyfkjf@kygk.com 890
-peter peter@jhf.com 8878
+peter peter@jhflj.edu.com 8878
+lkh saadsaad@gmail.com 3038
 " > $file
 
+# while loop to read file line by line
 while read line; do
 
 #if condition to check that three records to be checked have all required details (name , email and ID)
@@ -21,11 +23,12 @@ while read line; do
         name=$(echo "$line" | awk '{print $1}')
         email=$(echo "$line" | awk '{print $2}')
         id=$(echo "$line" | awk '{print $3}')
-        # check if the record conatins valid FQDN (Email)
-        fqdn="^(([A-Za-z0-9]+((\.|\-|\_|\+)?[A-Za-z0-9]?)*[A-Za-z0-9]+)|[A-Za-z0-9]+)@(([A-Za-z0-9]+)+((\.|\-|\_)?([A-Za-z0-9]+)+)*)+\.([A-Za-z]{2,})+$"
-        if [[ $email =~ ${fqdn} ]]
+        fqdn="^(([A-Za-z0-9]+((\.|\-|\_|\+)?[A-Za-z0-9]?)*[A-Za-z0-9]+)|
+                [A-Za-z0-9]+)@(([A-Za-z0-9]+)+((\.|\-|\_)?([A-Za-z0-9]+)+)*)+\.([A-Za-z]{2,})+$"
+# if condition to check if the record conatins valid FQDN (Email)
+        if [[ $email =~ $fqdn ]]
         then
-        # Check whether ID is ODD or EVEN
+# if condition to check whether ID is ODD or EVEN
             if [[ $id%2 -eq 0 ]]
             then
                 echo " $id of $email is even number"
